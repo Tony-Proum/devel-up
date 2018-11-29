df01e052546aaf1abddbc8a80bfd34388fa132ea
Tony Proum <t.proum@gmail.com>
2018-11-29 23:05:23 +0100

    feat(CLI): first CLI operation

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
+fn main() {
+    let matches = App::new("devel-up")
+        .about("Helps devel'upers to work with their favorite ecosystem")
+        .author("Tony Proum <t.proum@gmail.com>")
+        .version("0.0.1")
+        .arg(Arg::with_name("OPERATION")
+            .required(true)
+            .takes_value(true)).get_matches();
+    let operation = matches.value_of("OPERATION").unwrap();
+    println!("my operation is {}", operation);
+}
