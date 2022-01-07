db2b57585039ee0f83ea1453c4f8fa850e8e362f
Tone <t.proum@gmail.com>
2022-01-07 23:32:56 +0100

    :goal_net: enhance error handling

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -8,12 +8,18 @@ mod operations;
 use operations::{exec, Operations};
 
 fn main() {
-    let matches = App::new("devel-up")
+    let mut app = App::new("devel-up")
         .about("Helps devel'upers to work with their favorite ecosystem")
         .author("Tony Proum <t.proum@gmail.com>")
         .version("0.0.1")
-        .arg(Arg::with_name("OPERATION").required(true).takes_value(true))
-        .get_matches();
-    let operation = Operations::from_str(matches.value_of("OPERATION").unwrap()).unwrap();
-    exec(operation);
+        .arg(Arg::with_name("OPERATION").required(true).takes_value(true));
+    let operation = Operations::from_str(app.clone().get_matches().value_of("OPERATION").unwrap());
+    match operation {
+        Ok(_) => {
+            exec(operation.unwrap());
+        }
+        Err(_) => {
+            app.print_help().expect("help should be print");
+        }
+    }
 }
